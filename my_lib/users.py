8fdd4aa5c2e86537f8d0be670aa3a8e70786b61b
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-02-28 13:48:00 +0100

    docs: Updates class naming

--- my_lib/users.py
+++ my_lib/users.py
@@ -3,10 +3,10 @@
 # This program is licensed under the Apache License 2.0.
 # See LICENSE or go to <https://www.apache.org/licenses/LICENSE-2.0> for full license details.
 
-__all__ = ["Greeter"]
+__all__ = ["userGreeter"]
 
 
-class Greeter:
+class userGreeter:
 	"""Implements a user handling agent
 
 	Args:
