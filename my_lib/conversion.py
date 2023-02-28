a4e9be0de3b3aa557f16127c4fac9e52ffc01cff
F-G Fernandez <26927750+frgfm@users.noreply.github.com>
2023-02-28 13:27:19 +0100

    feat: Adds a message logging function

--- my_lib/conversion.py
+++ my_lib/conversion.py
@@ -5,7 +5,7 @@
 
 from typing import List
 
-__all__ = ["convert_fahrenheit_to_celsius"]
+__all__ = ["convert_fahrenheit_to_celsius", "log_message"]
 
 
 def convert_fahrenheit_to_celsius(input_temperatures: List[float]) -> List[float]:
@@ -19,3 +19,13 @@ def convert_fahrenheit_to_celsius(input_temperatures: List[float]) -> List[float
     """
 
     return [(fahrenheit_temp - 32) * 5 / 9 for fahrenheit_temp in input_temperatures]
+
+
+def log_message(message):
+	"""Logs a message
+
+	Args:
+		message: a string
+	"""
+	import logging
+	logging.info(message)
